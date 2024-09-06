48bd3aaf837adce99fef13815a0b1d080d1f1a79
Maxence LARMET <maxencelarmet@orange.fr>
2024-09-07 00:35:53 +0200

    update: clean code

--- backend/app.js
+++ backend/app.js
@@ -23,15 +23,15 @@ app.use(
 app.use(express.json());
 app.use(express.static("public"));
 
-for (let route of routes) {
-	app.use("/api/" + route.path, require(`./routes/${route.router}`));
+for (const route of routes) {
+	app.use(`/api/${route.path}`, require(`./routes/${route.router}`));
 }
 
 app.use(express.static(path.join(__dirname, "dist")));
-app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, "dist", "index.html")));
+app.get("/*", (req, res) => res.sendFile(path.resolve(__dirname, "dist", "index.html")));
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function (req, res, _next) {
 	res.status(404).send({
 		code: 404,
 		error: "route not found",
